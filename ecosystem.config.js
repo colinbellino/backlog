8cbbb77d83e30c2e2a7774a7f30fce5cb4aec070
Colin Bellino <bellino.colin@gmail.com>
2021-09-03 17:13:47 +0200

    Quick and dirty fix so PM2 doesn't create new windows every time we reload an app
    More infos: https://github.com/Unitech/pm2/issues/2182#issuecomment-629601292

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,14 @@ module.exports = {
     {
       name: "api",
       script: "./api/.dist/api.js",
+      watch: ["./api/.dist"],
+      instances: 1,
+      exec_mode: "cluster",
     },
     {
       name: "web",
       script: "serve",
+      watch: ["./web/.dist", "./web/index.html"],
       env: {
         PM2_SERVE_PATH: "./web",
         PM2_SERVE_PORT: 9999,
