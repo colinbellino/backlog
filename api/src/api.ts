788f9af7deafd3bb9aacfbb394770a27d9141687
Colin Bellino <me@colinbellino.com>
2021-09-06 00:31:14 +0200

    Play around with WebSockets

--- api/src/api.ts
+++ api/src/api.ts
@@ -1,14 +1,105 @@
 import * as http from "http";
+import * as WebSocket from "ws";
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const API_HOSTNAME = "127.0.0.1";
+const API_PORT = 3000;
+const WS_PORT = 3001;
 
-const server = http.createServer((_req, res) => {
+const wss = new WebSocket.Server({ port: WS_PORT });
+
+const dummyMessages = [
+  "Morbi et neque iaculis, ultricies nisl et, posuere arcu.",
+  "Nam vulputate eget nisl tempus elementum.",
+  "Fusce laoreet scelerisque odio quis venenatis.",
+  "Ut nec metus neque.",
+  "Aenean commodo mauris et ante luctus viverra.",
+  "Aliquam in leo vestibulum, iaculis ex sed, laoreet eros.",
+  "Vivamus aliquam ex eget tortor posuere, commodo fermentum dolor congue.",
+  "Proin sodales pulvinar mauris.",
+  "Morbi fringilla porttitor finibus.",
+  "Donec ipsum elit, suscipit auctor tortor sit amet, eleifend facilisis augue.",
+  "Mauris condimentum justo vel viverra aliquet.",
+  "Morbi quis leo purus.",
+  "Sed justo eros, pretium quis ligula ac, consequat tempus mauris.",
+  "Suspendisse potenti.",
+  "Aenean congue est nec nisi facilisis, molestie congue enim auctor.",
+  "Cras vulputate mi nec felis tempor condimentum.",
+  "Aliquam a aliquam eros.",
+  "Praesent nec tortor sit amet nisi lobortis facilisis.",
+  "Maecenas scelerisque dui id nibh porttitor, eu porta lectus volutpat.",
+  "Mauris hendrerit quam nunc, id imperdiet magna maximus non.",
+  "Etiam auctor, velit et laoreet hendrerit, erat metus rhoncus felis, nec lacinia erat augue non sem.",
+  "Ut ac posuere justo.",
+  "Fusce aliquam pretium elit eu laoreet.",
+  "Curabitur rutrum sagittis magna at posuere.",
+];
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    broadcastToAll(message.toString());
+  });
+});
+
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/json");
+
+  if (req.url == "/test-message") {
+    broadcastToAll(
+      dummyMessages[Math.floor(Math.random() * dummyMessages.length)]!
+    );
+
+    res.end(`{ "message": "Message broadcasted to all clients!" }`);
+    return;
+  }
+
+  if (req.url == "/test-binary") {
+    const array = new Float32Array(5);
+    for (var i = 0; i < array.length; ++i) {
+      array[i] = i + 1;
+    }
+
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(array, { binary: true });
+      }
+    });
+    res.end(`{ "message": "Message broadcasted to all clients!" }`);
+    return;
+  }
+
+  if (req.url == "/test-json") {
+    const json = {
+      type: dummyMessages[Math.floor(Math.random() * dummyMessages.length)]!,
+    };
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(json));
+      }
+    });
+
+    res.end(`{ "message": "Message broadcasted to all clients!" }`);
+    return;
+  }
+
   res.end(`{ "message": "Hello World!" }`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(API_PORT, API_HOSTNAME, () => {
+  console.log(`[Core] Starting API at http://${API_HOSTNAME}:${API_PORT}/`);
+  console.log(`[Core] Starting WebSocket at ws://${API_HOSTNAME}:${WS_PORT}/`);
 });
+
+setInterval(function () {
+  broadcastToAll(
+    dummyMessages[Math.floor(Math.random() * dummyMessages.length)]!
+  );
+}, Math.floor(Math.random() * 10000));
+
+function broadcastToAll(message: string) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
--- web/app.ts
+++ web/app.ts
@@ -1,1 +1,76 @@
-console.log(`Hello world`);
+const WS_RECONNECT_COOLDOWN = 5000;
+
+const locale = "en-US";
+const dateFormat: Intl.DateTimeFormatOptions = {
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+};
+
+const messageElement: HTMLInputElement = document.querySelector("#message")!;
+const formElement: HTMLFormElement = document.querySelector("#sendMessage")!;
+
+let ws = createWS();
+
+const state = {
+  running: true,
+  nextReconnect: 0,
+};
+
+function createWS() {
+  const ws = new WebSocket("ws://localhost:3001");
+
+  ws.onopen = function () {
+    appendMessage("Connection opened.");
+  };
+
+  ws.onclose = function () {
+    appendMessage("Connection closed.");
+    state.nextReconnect = Date.now() + WS_RECONNECT_COOLDOWN;
+  };
+
+  ws.onmessage = function (event) {
+    appendMessage(event.data);
+  };
+
+  ws.onerror = function (event) {
+    console.error("Socket error: ", event);
+    ws.close();
+  };
+
+  return ws;
+}
+
+function appendMessage(message: string) {
+  const li = document.createElement("li");
+  const date = new Date().toLocaleDateString(locale, dateFormat);
+  li.textContent = `${date}: ${message}`;
+  document.querySelector("body")!.append(li);
+
+  console.log(message);
+}
+
+function onSubmit(event: Event) {
+  event.preventDefault();
+
+  if (messageElement.value) {
+    sendMessage(messageElement.value);
+  }
+}
+
+function sendMessage(message: string) {
+  ws.send(message);
+}
+
+function tick() {
+  if (state.nextReconnect > 0 && Date.now() >= state.nextReconnect) {
+    ws = createWS();
+    state.nextReconnect = 0;
+  }
+
+  window.requestAnimationFrame(tick);
+}
+
+window.requestAnimationFrame(tick);
+
+formElement.addEventListener("submit", onSubmit);
